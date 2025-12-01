df55cc365b70d089947a88a3f64da71e02a7de80
Anamul Hassan <itsanamulhassan@gmail.com>
2025-12-02 05:21:50 +0600

    feat(wallet): implement wallet module with models, schemas, services, and types

---
+++ src/app/modules/wallet/wallet.models.ts
@@ -0,0 +1,45 @@
+import { HydratedDocument, InferSchemaType, model, Schema } from "mongoose";
+import { WalletTransactionTypeEnum } from "./wallet.types";
+
+const WalletTransactionSchema = new Schema(
+  {
+    driver: {
+      type: Schema.Types.ObjectId,
+      ref: "Drivers",
+      required: [true, "Driver ID is required."],
+    },
+    ride: {
+      type: Schema.Types.ObjectId,
+      ref: "Rides",
+      required: [true, "Ride ID is required."],
+    },
+    type: {
+      type: String,
+      enum: WalletTransactionTypeEnum,
+      required: true,
+    },
+
+    amount: {
+      type: Number,
+      required: [true, "Amount is required."],
+    },
+
+    payoutBatchId: {
+      type: Schema.Types.ObjectId,
+      ref: "Payouts",
+      default: null,
+    },
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export type WalletTransaction = InferSchemaType<typeof WalletTransactionSchema>;
+export type WalletTransactionDocument = HydratedDocument<WalletTransaction>;
+
+export const WalletTransactions = model<WalletTransaction>(
+  "WalletTransactions",
+  WalletTransactionSchema
+);
