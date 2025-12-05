0edb2301a26af1cac5cc763944398aa6c11efc26
Anamul Hassan <itsanamulhassan@gmail.com>
2025-12-05 07:17:07 +0600

    refactor(rider): update route parameter from 'id' to 'userId' for clarity

--- src/app/modules/rider/rider.services.ts
+++ src/app/modules/rider/rider.services.ts
@@ -7,14 +7,14 @@ import { UpdateRiderDTO } from "./rider.types";
 
 const updateRider = async (req: Request) => {
   const payload = req.body as UpdateRiderDTO;
-  const id = req.params.id;
+  const userId = req.params.userId;
 
-  const rider = await Riders.findOne({ user: id });
+  const rider = await Riders.findOne({ user: userId });
   if (!rider) {
     throw new AppError(message("notFound", "rider"), StatusCodes.NOT_FOUND);
   }
   return await Riders.findOneAndUpdate(
-    { user: id },
+    { user: userId },
     {
       favoriteLocations: payload.favoriteLocations,
       paymentMethods: payload.paymentMethods,
